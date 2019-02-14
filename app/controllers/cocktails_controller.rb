9512485d85979dbdca34b6da80faa10cf147db3d
Edouard Vaudour <edouard.vaudour@icloud.com>
2019-02-14 15:35:17 +0100

    Cocktails

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,41 @@
+class CocktailsController < ApplicationController
+
+def index
+    @cocktails = Cocktail.all
+  end
+
+def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new           # GET /cocktails/new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  #def edit          # GET /cocktails/:id/edit
+  #end
+
+  #def update        # PATCH /cocktails/:id
+  #end
+
+ def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+  end
+
+private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :ingredients, :doses)
+  end
+
+end
